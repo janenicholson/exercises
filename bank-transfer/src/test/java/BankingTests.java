c5950ef9463389251aac6c4cffa6ab44cf0a9a4c
Jane Nicholson <jane.nicholson@masabi.com>
2016-05-27 11:39:25 +0100

    Remove unused stuff

--- bank-transfer/src/test/java/BankingTests.java
+++ bank-transfer/src/test/java/BankingTests.java
@@ -1,28 +1,8 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class BankingTests extends Account {
-
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
 
     @Test
     public void transfer_amount_from_payer_to_payee() {
